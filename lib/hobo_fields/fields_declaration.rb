a1b46cc875fe4eb046c5c77b6fc79abc095d0e21
Scott Bronson <bronson@rinspin.com>
2010-02-10 03:05:31 -0800

    Don't use hobosupport's classy_module

--- lib/hobo_fields/fields_declaration.rb
+++ lib/hobo_fields/fields_declaration.rb
@@ -1,20 +1,25 @@
 module HoboFields
 
-  FieldsDeclaration = classy_module do
+  module FieldsDeclaration
+    def self.included(base)
+      base.extend ClassMethods
+    end
 
-    def self.fields(include_in_migration = true, &b)
-      # Any model that calls 'fields' gets a bunch of other
-      # functionality included automatically, but make sure we only
-      # include it once
-      include HoboFields::ModelExtensions unless HoboFields::ModelExtensions.in?(included_modules)
-      @include_in_migration ||= include_in_migration
+    module ClassMethods
+      def fields(include_in_migration = true, &b)
+        # Any model that calls 'fields' gets a bunch of other
+        # functionality included automatically, but make sure we only
+        # include it once
+        include HoboFields::ModelExtensions unless HoboFields::ModelExtensions.in?(included_modules)
+        @include_in_migration ||= include_in_migration
 
-      if b
-        dsl = HoboFields::FieldDeclarationDsl.new(self)
-        if b.arity == 1
-          yield dsl
-        else
-          dsl.instance_eval(&b)
+        if b
+          dsl = HoboFields::FieldDeclarationDsl.new(self)
+          if b.arity == 1
+            yield dsl
+          else
+            dsl.instance_eval(&b)
+          end
         end
       end
     end
