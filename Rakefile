tslocke/hobo

rake test:doctest -- don't depend on the shell to expand our file list

Rakefile
@@ -28,8 +28,9 @@ Rake::TestTask.new(:test) { |t|
 namespace "test" do
   desc "Run the doctests"
   task :doctest do |t|
+    files=Dir['doctest/*.rdoctest'].map {|f| File.expand_path(f)}.join(' ')
     # note, tests in doctest/hobo/ are out of date
-    exit(1) if !system("#{RUBYDOCTEST} doctest/*.rdoctest")
+    exit(1) if !system("#{RUBYDOCTEST} #{files}")
   end
 end
 

bronson/rich-models

Rakefile
@@ -13,7 +13,8 @@ require 'hobofields'
 namespace "test" do
   desc "Run the doctests"
   task :doctest do |t|
-    exit(1) if !system("#{RUBYDOCTEST} test/*.rdoctest")
+    files=Dir['test/*.rdoctest'].map {|f| File.expand_path(f)}.join(' ')
+    exit(1) if !system("#{RUBYDOCTEST} #{files}")
   end
 
   desc "Run the unit tests"